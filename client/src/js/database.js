b4522d6c67d945ddfd69edc74d4c10cad147f7ee
rkutsel <roman.kutsel@docusign.com>
2022-07-09 14:21:15 -0700

    initial boilerplate code

---
+++ client/src/js/database.js
@@ -0,0 +1,19 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+	openDB("jate", 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains("jate")) {
+				console.log("jate database already exists");
+				return;
+			}
+			db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+			console.log("jate database created");
+		},
+	});
+
+export const putDb = async (content) => console.error("putDb not implemented");
+
+export const getDb = async () => console.error("getDb not implemented");
+
+initdb();
