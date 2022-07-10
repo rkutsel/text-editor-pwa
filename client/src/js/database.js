e80d09d112ae81de82829e23240213c03ddd000f
rkutsel <roman.kutsel@docusign.com>
2022-07-09 23:40:31 -0700

    db put && db get functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,13 +7,32 @@ const initdb = async () =>
 				console.log("jate database already exists");
 				return;
 			}
-			db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+			db.createObjectStore("jate", { keyPath: "id", autoIncrement: false });
 			console.log("jate database created");
 		},
 	});
 
-export const putDb = async (content) => console.error("putDb not implemented");
+export const putDb = async (content) => {
+	console.log("Database PUT");
+	const jateDb = await openDB("jate", 1);
+	const tx = jateDb.transaction("jate", "readwrite");
+	const store = tx.objectStore("jate");
+	const request = store.put({ id: 1, jate: content });
+	const result = await request;
+	console.log(`Database Updated! 🚀🚀🚀, >>${result}<<`);
+};
 
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+	console.log("GET all from the database");
+	const jateDb = await openDB("jate", 1);
+	const tx = jateDb.transaction("jate", "readonly");
+	const store = tx.objectStore("jate");
+	const request = store.get(1);
+	const result = await request;
+	console.log("result.value", result);
+	if (result) {
+		return result.jate;
+	}
+};
 
 initdb();
